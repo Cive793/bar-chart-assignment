ce061cbe1944a199992704dac56708decfb5f8ba
peterstastny <peter.stastny7@gmail.com>
2021-09-04 17:11:04 +0200

    1 bar working

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 const customersArray = [0, 1, 16, 2, 9, 6, 22, 27, 31, 7, 1, 8, 14, 29, 22, 16, 7, 4, 9, 18, 27, 11, 6, 23, 32, 21, 17, 5, 8, 17, 24, 9, 6, 15, 8, 1, 13, 7, 9, 14];
 console.log(customersArray);
-let counter = 0;
+//let counter = 0;
 let queueSize = getNumberOfCustomers();
 let individualBar = document.querySelector(".individual-bar");
 
@@ -16,8 +16,8 @@ function getNumberOfCustomers() {
 loop();
 
 function loop() {
-  console.log("loop");
-  counter++;
+  //console.log("loop");
+  //counter++;
   customersArray.unshift(queueSize);
   setTimeout(loop, 1000);
   customersArray.slice(1, 41);
@@ -25,6 +25,9 @@ function loop() {
 }
 
 function setBarHeight() {
-  console.log("setBarHeight");
-  individualBar.style.height = queueSize;
+  //console.log("setBarHeight");
+
+  for (let counter = 0; counter < 40; counter++) {
+    individualBar.style.height = queueSize * 3 + "px";
+  }
 }
