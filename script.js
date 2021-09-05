60877b26e365a7285b2faff6ab317404cce27df3
peterstastny <peter.stastny7@gmail.com>
2021-09-05 17:03:14 +0200

    1 bar changing automatically

--- script.js
+++ script.js
@@ -3,31 +3,30 @@
 const customersArray = [0, 1, 16, 2, 9, 6, 22, 27, 31, 7, 1, 8, 14, 29, 22, 16, 7, 4, 9, 18, 27, 11, 6, 23, 32, 21, 17, 5, 8, 17, 24, 9, 6, 15, 8, 1, 13, 7, 9, 14];
 console.log(customersArray);
 //let counter = 0;
-let queueSize = getNumberOfCustomers();
+//let queueSize;
 let individualBar = document.querySelector(".individual-bar");
-
-getNumberOfCustomers();
-
-function getNumberOfCustomers() {
-  //console.log(queueSize);
-  return Math.floor(Math.random() * 32);
-}
 
 loop();
 
 function loop() {
-  //console.log("loop");
+  console.log("loop");
   //counter++;
+  let queueSize = Math.floor(Math.random() * 32);
   customersArray.unshift(queueSize);
-  setTimeout(loop, 1000);
+
+  individualBar.style.height = queueSize * 3 + "px";
+
   customersArray.slice(1, 41);
-  setBarHeight();
+  console.log(queueSize);
+  setTimeout(loop, 1000);
+
+  //setBarHeight();
 }
 
-function setBarHeight() {
+/* function setBarHeight() {
   //console.log("setBarHeight");
 
   for (let counter = 0; counter < 40; counter++) {
     individualBar.style.height = queueSize * 3 + "px";
   }
-}
+} */
