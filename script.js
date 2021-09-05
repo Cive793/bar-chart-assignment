c27f564e49b0db54b705c1823fb66a554a8ff4fc
peterstastny <peter.stastny7@gmail.com>
2021-09-05 19:42:54 +0200

    only targeting other bars missing

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
 "use strict";
 
-const customersArray = [0, 1, 16, 2, 9, 6, 22, 27, 31, 7, 1, 8, 14, 29, 22, 16, 7, 4, 9, 18, 27, 11, 6, 23, 32, 21, 17, 5, 8, 17, 24, 9, 6, 15, 8, 1, 13, 7, 9, 14];
-console.log(customersArray);
-//let counter = 0;
-//let queueSize;
+const customersArray = [12, 1, 16, 2, 9, 6, 22, 27, 31, 7, 1, 8, 14, 29, 22, 16, 7, 4, 9, 18, 27, 11, 6, 23, 32, 21, 17, 5, 8, 17, 24, 9, 6, 15, 8, 1, 13, 7, 9, 14];
+
+let counter = 0;
+//let classCounter = 0;
+let arrayLength = 40;
 let individualBar = document.querySelector(".individual-bar");
+/* "1" "2" "3" "4" "5" "6" "7" "8" "9" "10" "11" "12" "13" "14" "15" "16" "17" "18" "19" "20" "21" "22" "23" "24" "25" "26" "27" "28" "29" "30" "31" "32" "33" "34" "35" "36" "37" "38" "39" "40" */
 
 loop();
 
 function loop() {
   console.log("loop");
-  //counter++;
+  counter++;
+  //classCounter++;
   let queueSize = Math.floor(Math.random() * 32);
-  customersArray.unshift(queueSize);
-
-  individualBar.style.height = queueSize * 3 + "px";
-
-  customersArray.slice(1, 41);
   console.log(queueSize);
-  setTimeout(loop, 1000);
+
+  if (counter < arrayLength) {
+    loop();
+  } else {
+    customersArray.unshift(queueSize);
+    customersArray.splice(39, 1);
+    console.log(customersArray);
+  }
+  //setTimeout(loop, 1000);
 
   //setBarHeight();
+
+  individualBar.style.height = queueSize * 3 + "px";
 }
 
 /* function setBarHeight() {
