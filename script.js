359c0c00275acddeacae247dc1e1364ffe14647a
peterstastny <peter.stastny7@gmail.com>
2021-09-08 23:24:11 +0200

    bars changing past 40

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 const customersArray = [12, 1, 16, 2, 9, 6, 22, 27, 31, 7, 1, 8, 14, 29, 22, 16, 7, 4, 9, 18, 27, 11, 6, 23, 32, 21, 17, 5, 8, 17, 24, 9, 6, 15, 8, 1, 13, 7, 9, 14];
 
 let counter = 0;
+let newCounter = 0;
 const maxNumberOfLoops = customersArray.length;
 let individualBar = document.querySelector("#individual-bar" + counter);
 
@@ -23,11 +24,14 @@ function loop() {
     loop();
   } else {
     let queueSize = Math.floor(Math.random() * 32);
-    //console.log(queueSize);
+    let newBarHeight = customersArray[newCounter];
+    newCounter++;
+    individualBar = document.querySelector("#individual-bar" + newCounter);
+    individualBar.style.height = newBarHeight * 6 + "px";
     customersArray.push(queueSize);
     customersArray.shift();
     //console.log(customersArray);
-    individualBar.style.height = customersArray[0] * 6 + "px";
+
     //console.log(individualBar);
 
     setTimeout(loop, 1000);
